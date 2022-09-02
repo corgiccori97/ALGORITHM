abd006288fafef4aaf7c9ac054f999f71cc06898
corgiccori97 <tkdgmlgg7@gmail.com>
2022-09-02 16:53:52 +0900

    a

--- 수학/1182.py
+++ 수학/1182.py
@@ -1,5 +1,14 @@
 import sys
+from itertools import combinations
 
 N, S = map(int, sys.stdin.readline().split())
 arr = list(map(int, sys.stdin.readline().split()))
 
+cnt = 0
+for i in range(1, N+1):
+    com = combinations(arr, i)
+    for c in list(com):
+        if sum(c) == S:
+            cnt += 1
+
+print(cnt)
---
+++ 수학/1182_dfs.py
@@ -0,0 +1,20 @@
+import sys
+
+def dfs(idx, sum):
+    global cnt
+    if idx >= N:
+        return
+    sum += arr[idx]
+    if sum == S:
+        cnt += 1
+    dfs(idx + 1, sum - arr[idx])
+    dfs(idx + 1, sum)
+
+N, S = map(int, sys.stdin.readline().split())
+arr = list(map(int, sys.stdin.readline().split()))
+cnt = 0
+
+dfs(0, 0)
+print(cnt)
+
+# 이해가안돼요~!!!!!!!
