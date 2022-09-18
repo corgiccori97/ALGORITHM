6cf29fa41288bfb14f03498cb3ab9422019fef9c
corgiccori97 <tkdgmlgg7@gmail.com>
2022-09-18 23:55:29 +0900

    a

--- 수학/(~ing)15663.py
+++ 수학/(~ing)15663.py
@@ -2,16 +2,22 @@ import sys
 
 N, M = map(int, sys.stdin.readline().split())
 arr = sorted(list(map(int, sys.stdin.readline().split())))
+visited = [False] * N
 temp = []
+
 
 def dfs():
     if len(temp) == M:
         print(*temp)
         return
-    for a in arr:
-        if a not in temp:
-            temp.append(a)
+    same = 0
+    for i in range(N):
+        if not visited[i] and same != arr[i]:
+            visited[i] = True
+            temp.append(arr[i])
+            same = arr[i]
             dfs()
+            visited[i] = False
             temp.pop()
 
 
