37d4a0d1de811c77d2b7ead2ffafb9111afdade3
corgiccori97 <tkdgmlgg7@gmail.com>
2022-10-11 14:54:29 +0900

    dfs

--- DFS/11724.py
+++ DFS/11724.py
@@ -1,18 +1,27 @@
-def dfs(v, depth):
-    visited[v] = 1
-    for i in range(1, n + 1):
-        if graph[v][i] == 0 and visited[i] == 0:
-            dfs(v, depth + 1)
-            count += 1
+import sys
+# 재귀 허용치가 넘어가면 런타임에러 일어나기 때문에 허용 범위 넓혀주기
+sys.setrecursionlimit(10**7)
+input = sys.stdin.readline
 
-n, m = int(input().split())
-graph = [[0] * (n + 1) for _ in range(n + 1)]
+def dfs(v):
+    visited[v] = 1
+    for i in graph[v]:
+        if visited[i] == 0:
+            dfs(i)
+
+n, m = map(int, input().split())
+graph = [[] for _ in range(n + 1)]
+visited = [0] * (n + 1)
+cnt = 0
 
 for _ in range(m):
     a, b = map(int, input().split())
-    graph[a][b] = graph[b][a] = 1
+    graph[a].append(b)
+    graph[b].append(a)
 
-visited = [0] * (n + 1)
-count = 0
+for i in range(1, n + 1):
+    if visited[i] == 0:
+        cnt += 1
+        dfs(i)
 
-dfs()
+print(cnt)
