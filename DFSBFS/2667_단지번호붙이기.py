cdaec3a0da3a94293a0ef2ead6e3beb7e978f213
corgiccori97 <tkdgmlgg7@gmail.com>
2022-10-22 20:13:14 +0900

    단지번호붙이기dfs

--- DFSBFS/2667_단지번호붙이기.py
+++ DFSBFS/2667_단지번호붙이기.py
@@ -1,21 +1,29 @@
 import sys
 input = sys.stdin.readline
 
-def dfs(x, y):
-    global gagu
-    visited[x][y] = 1
-    for nx in range(x, n + 1):
-        for ny in range(y, n + 1):
-            if graph[nx][ny] and not visited[nx][ny]:
-                dfs(nx, ny)
-                gagu += 1
-    return gagu
+dx = [0, 0, -1, 1]
+dy = [1, -1, 0, 0]
 
+def dfs(x, y):
+    if x < 0 or x >= n or y < 0 or y >= n:
+        return False
+    if graph[x][y] == 1:
+        graph[x][y] = 0
+        global gagu
+        gagu += 1
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            dfs(nx, ny)
+        return True
+    return False
+        
 n = int(input())
 graph = [[0] * (n + 1) for _ in range(n + 1)]
 visited = graph.copy()
 apt = 0
 gagu = 0
+gagus = []
 
 for i in range(n):
     arr = list(input())
@@ -25,6 +33,10 @@ for i in range(n):
 
 for i in range(n):
     for j in range(n):
-        if graph[i][j] == 1:
-            dfs(i, j)
+        if dfs(i, j) == True:
+            gagus.append(gagu)
             apt += 1
+            gagu = 0
+
+print(apt)
+[print(gagu) for gagu in sorted(gagus)]
